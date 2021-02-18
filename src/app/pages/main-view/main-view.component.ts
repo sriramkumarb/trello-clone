d7153a08c58dcceb7e2dfcf58953406c1dacbb14
Sriram <sriram9159541445@gmail.com>
2021-02-18 14:02:55 +0530

    feat : complete manual trello board finished

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -9,23 +11,34 @@ export class MainViewComponent implements OnInit {
 
   constructor() { }
 
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
+
   ngOnInit(): void {
   }
-
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
