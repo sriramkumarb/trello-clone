46364c5ac1e8fb14a0da978fddc56588637ae049
Sriram <sriram9159541445@gmail.com>
2021-02-18 22:28:41 +0530

    feat(server) : initial setup & api for boards done

---
+++ server/db/models/board.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const BoardSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    // with auth
+    // _userId: {
+    //     type: mongoose.Types.ObjectId,
+    //     required: true
+    // }
+
+})
+
+const Board = mongoose.model('Board', BoardSchema);
+
+module.exports = { Board }
---
+++ server/db/models/index.js
@@ -0,0 +1,8 @@
+// Load in the mongoose models
+const { Board } = require('./board.model');
+const { Task } = require('./task.model');
+
+module.exports = {
+    Board,
+    Task
+}
